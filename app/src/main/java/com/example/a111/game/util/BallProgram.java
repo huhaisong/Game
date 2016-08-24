77912275eb8b00d94cd76597d31aa541aa285757
huhaisong <123>
2016-08-24 10:17:20 +0800

    Rejected commit: Default

--- app/src/main/java/com/example/a111/game/util/BallProgram.java
+++ app/src/main/java/com/example/a111/game/util/BallProgram.java
@@ -7,20 +7,16 @@ import static com.example.a111.game.util.ShaderUtil.createProgram;
 /**
  * Created by 111 on 2016/8/23.
  */
-public class VRProgram {
+public class BallProgram {
 
+    private static BallProgram mBallProgram;
 
     public int mProgram;//自定义渲染管线着色器程序id
     public int muMVPMatrixHandle;//总变换矩阵引用
     public int maPositionHandle; //顶点位置属性引用
     public int maTexCoorHandle; //顶点纹理坐标属性引用
-    public int muMMatrixHandle;
 
-    public int maCameraHandle; //摄像机位置属性引用
-    public int maNormalHandle; //顶点法向量属性引用
-    public int maLightLocationHandle;//光源位置属性引用
-
-    public String mVertexShader = "uniform mat4 uMVPMatrix; //总变换矩阵\n" +
+    private String mVertexShader = "uniform mat4 uMVPMatrix; //总变换矩阵\n" +
             "attribute vec3 aPosition;  //顶点位置\n" +
             "attribute vec2 aTexCoor;    //顶点纹理坐标\n" +
             "varying vec2 vTextureCoord;  //用于传递给片元着色器的变量\n" +
@@ -29,7 +25,7 @@ public class VRProgram {
             "   gl_Position = uMVPMatrix * vec4(aPosition,1); //根据总变换矩阵计算此次绘制此顶点位置\n" +
             "   vTextureCoord = aTexCoor;//将接收的纹理坐标传递给片元着色器\n" +
             "}                      ";
-    public String mFragmentShader = "precision mediump float;\n" +
+    private String mFragmentShader = "precision mediump float;\n" +
             "uniform sampler2D sTexture;//纹理内容数据\n" +
             "varying vec2 vTextureCoord; //接收从顶点着色器过来的参数\n" +
             "varying vec4 vambient;\n" +
@@ -42,8 +38,7 @@ public class VRProgram {
             "   //给此片元颜色值\n" +
             "}              ";
 
-
-    public VRProgram() {
+    private BallProgram() {
         mProgram = createProgram(mVertexShader, mFragmentShader);
         //获取程序中顶点位置属性引用id
         maPositionHandle = GLES20.glGetAttribLocation(mProgram, "aPosition");
@@ -51,5 +46,13 @@ public class VRProgram {
         maTexCoorHandle = GLES20.glGetAttribLocation(mProgram, "aTexCoor");
         //获取程序中总变换矩阵引用id
         muMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+    }
+
+    public static BallProgram getInstance() {
+
+        if (mBallProgram == null) {
+            mBallProgram = new BallProgram();
+        }
+        return mBallProgram;
     }
 }
